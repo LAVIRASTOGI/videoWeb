8a894816a3c9893eba37dd8bc214f564aaf65d9f
LaviRastogi <1993lavirastogi@gmail.com>
2024-10-24 02:12:57 +0530

    meeting

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,28 @@
-import Hero from '@/components/hero'
+
 import React from 'react'
 
 function Home() {
+    const now = new Date();
+
+    const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+    const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+
     return (
-        <div className='flex flex-col gap-8'>
-            <Hero />
+        <div className='size-full flex flex-col gap-10 bg-dark-2 pt-[120px] p-10'>
+            {/* hero */}
+            <div className='w-full h-[60vh] bg-cover bg-hero rounded-2xl'>
+                <div className='flex justify-end'>
+                    <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11 gap-6">
+                        <h1 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
+                            Upcoming Meeting at: 12:30 PM
+                        </h1>
+                        <div className="flex flex-col gap-4">
+                            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+                            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+                        </div>
+                    </div></div>
+
+            </div>
         </div>
     )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,7 +7,7 @@ import { SignedIn, UserButton } from '@clerk/nextjs'
 
 function Navbar() {
     return (
-        <div className='fixed top-0 right-0 w-screen z-50 flex justify-between px-6 py-4 
+        <div className='fixed top-0 right-0  w-screen z-50 flex justify-between px-6 py-4 
         lg:px-10 items-center bg-dark-1 backdrop-blur-md'>
             <div className='flex gap-2 items-center'>
                 <Image src={'/icons/video.jpg'} alt='TalentBridge' height={40} width={40} />
