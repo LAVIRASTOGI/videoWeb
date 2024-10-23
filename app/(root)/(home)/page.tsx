516bff48d125e98cdfae94e0e1e35e312562a189
LaviRastogi <1993lavirastogi@gmail.com>
2024-10-24 02:18:37 +0530

    hero session

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,10 +10,10 @@ function Home() {
     return (
         <div className='size-full flex flex-col gap-10 bg-dark-2 pt-[120px] p-10'>
             {/* hero */}
-            <div className='w-full h-[60vh] bg-cover bg-hero rounded-2xl'>
-                <div className='flex justify-end'>
+            <div className='w-full md:h-[60vh]  h-[50vh] bg-cover bg-hero rounded-2xl'>
+                <div className='flex justify-center md:justify-end h-full md:h-auto'>
                     <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11 gap-6">
-                        <h1 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
+                        <h1 className="glassmorphism max-w-[273px] rounded p-2 text-center text-base font-normal mx-auto">
                             Upcoming Meeting at: 12:30 PM
                         </h1>
                         <div className="flex flex-col gap-4">
