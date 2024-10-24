f971d40c8e8f1acab429c2f347cdc77b4d11b230
LaviRastogi <1993lavirastogi@gmail.com>
2024-10-24 12:07:27 +0530

    button

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -1,4 +1,5 @@
 
+'use client'
 import { cn } from '@/lib/utils';
 import Image from 'next/image'
 import React from 'react'
@@ -9,15 +10,16 @@ interface HeroCardProps {
     icon: string;
     backgroundColor: string;
     id: string | number;
-    // handleClick?: () => void;
+    handleClick?: () => void;
 
 }
 
-const HeroCard: React.FC<HeroCardProps> = ({ title, description, icon, id, backgroundColor }) => {
+const HeroCard: React.FC<HeroCardProps> = ({ title, description, icon, id, backgroundColor, handleClick }) => {
     return (
-        <div key={id} className={cn('flex flex-col justify-between w-full min-h-64 p-4 rounded-2xl cursor-pointer', backgroundColor)}>
+        <div key={id} className={cn('flex flex-col justify-between w-full min-h-64 p-4 rounded-2xl cursor-pointer', backgroundColor)}
+            onClick={handleClick}
+        >
             <div className='glassmorphism p-3 w-12 rounded-xl'
-            // onClick={handleClick}
             >
                 <Image src={icon} alt={title} width={32} height={32} />
             </div >
--- components/meetingtemplate.tsx
+++ components/meetingtemplate.tsx
@@ -1,38 +1,56 @@
 'use client'
 import { meetingTemplate } from '@/constant'
-
-// import React, { useState } from 'react'
+import React, { useState } from 'react'
 import HeroCard from './HomeCard'
-// import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import MeetingModal from './MeetingModal';
 
 function MeetingTemplate() {
-    // const router = useRouter();
-    // const [meetingState, setMeetingState] = useState<string>('');
+    const router = useRouter();
+    const [meetingState, setMeetingState] = useState<string>('');
 
+    const meetingcolor = (id: number) => {
+        const arr = ['bg-orange-1', 'bg-blue-1', 'bg-purple-1'];
+        return arr[id - 1];
+    }
+    const createMeeting = () => {
 
+    }
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-            {
-                meetingTemplate.map((item, index) => {
-                    return (<HeroCard key={index} title={item.title}
-                        description={item.descrption}
-                        icon={item.icon}
-                        id={item.id}
-                        backgroundColor={item.id === 1 ? 'bg-orange-1' : item.id === 2 ? 'bg-blue-1' : 'bg-purple-1'}
-                    // handleClick={() => setMeetingState(item.meeting)}
-                    />)
+        <>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+                {
+                    meetingTemplate.map((item, index) => {
+                        return (<HeroCard key={index} title={item.title}
+                            description={item.descrption}
+                            icon={item.icon}
+                            id={item.id}
+                            backgroundColor={meetingcolor(Number(item.id))}
+                            handleClick={() => setMeetingState(item.meeting)}
+                        />)
 
-                })
-            }
-            <HeroCard
-                icon="/icons/recordings.svg"
-                title="View Recordings"
-                description="Meeting Recordings"
-                backgroundColor="bg-yellow-1"
-                // handleClick={() => router.push('/recordings')}
-                id={4}
+                    })
+                }
+                <HeroCard
+                    icon="/icons/recordings.svg"
+                    title="View Recordings"
+                    description="Meeting Recordings"
+                    backgroundColor="bg-yellow-1"
+                    handleClick={() => router.push('/recordings')}
+                    id={4}
+                />
+            </div>
+            <MeetingModal
+                isOpen={meetingState === 'isInstantMeeting'}
+                onClose={() => setMeetingState('')}
+                title="Start an Instant Meeting"
+                className="text-center"
+                buttonText="Start Meeting"
+                handleClick={createMeeting}
             />
-        </div>
+        </>
+
+
     )
 }
 
